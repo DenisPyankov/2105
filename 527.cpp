9548ff63c387df0911a03586106eb05fdd73f89e
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-05-27 05:41:27 +0500

    Add files via upload

---
+++ 527.cpp
@@ -0,0 +1,37 @@
+﻿#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ifstream in("in.txt");
+	ofstream out("out.txt");
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		int c, d;
+		cin >> c >> d;
+		bool flag = 0;
+		while (b != 0) {
+			if (b > a) {
+				swap(a, b);
+			}
+			if (a == c && b == d) {
+				flag = 1;
+			}
+			a = a - b;
+			if (a == c && b == d) {
+				flag = 1;
+			}
+		}
+		if (flag) {
+			cout << "YES" << endl;
+		}
+		else {
+			cout << "NO" << endl;;
+		}
+	}
+}
